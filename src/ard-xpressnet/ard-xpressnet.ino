e3dad850f3ccd9f49f9ffa288243bb24758dc83e
Michael Petersen <railfan@drgw.net>
2017-02-22 23:05:21 -0700

    Renamed and added header information

--- src/ard-xpressnet/ard-xpressnet.ino
+++ src/ard-xpressnet/ard-xpressnet.ino
@@ -1,3 +1,24 @@
+/*************************************************************************
+Title:    ARD-XPRESSNET Arduino-based decoder for the Lenz XpressNet bus
+Authors:  Michael D. Petersen <railfan@drgw.net>
+          Nathan D. Holmes <maverick@drgw.net>
+File:     $Id: $
+License:  GNU General Public License v3
+
+LICENSE:
+    Copyright (C) 2017 Michael Petersen
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*************************************************************************/
+
 #include <util/atomic.h>
 
 #define BAUD 62500
